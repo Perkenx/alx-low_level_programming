dd69236ee589665ed4dda12e7dee2577b3ffc9e5
perkenx <perkenx@gmail.com>
2022-09-26 12:30:25 -0700

    Task 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -3,14 +3,10 @@
 
 
 /**
- *
- *  * print_chessboard - print the chessboard, 2d array
- *
- *   * @a: 2d array of chars
- *
- *    *Return: void
- *
- *     */
+ * print_chessboard - print the chessboard, 2d array
+ * @a: 2d array of chars
+ *Return: void
+ */
 
 void print_chessboard(char (*a)[8])
 {
